6229ca7caba1eefc8a3d0f7a994c2a9aef42f7ff
PhebeK <77545491+PhebeK@users.noreply.github.com>
2021-04-17 11:30:21 -0400

    Sense Display Project
    Completed Sensehat project

--- sense_display/app.py
+++ sense_display/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, current_app as app
 from sense_emu import SenseHat
+import sqlite3
 
 app = Flask(__name__)
 sense = SenseHat()
@@ -16,6 +17,20 @@ def done():
     sense.show_message(message)
     
     return render_template('done.html', message = message)
+
+
+@app.route('/all')
+def all():
+    conn = sqlite3.connect('./static/data/senseDisplay.db')
+    curs = conn.cursor()
+    bulletin = []
+    rows = curs.execute("SELECT * from messages")
+    for row in rows:
+        lines = {'message': row[0]}
+        bulletin.append(lines)
+    conn.close
+    return render_template('all.html', bulletin = bulletin)
+    
 
 if __name__ == '__main__':
     app.run(debug = True, host='0.0.0.0')
