9fd9c469084928d1efc8086285a18afe97d05ab6
PhebeK <77545491+PhebeK@users.noreply.github.com>
2021-05-01 14:03:07 -0400

    My_Tasks

--- my_tasks/app.py
+++ my_tasks/app.py
@@ -1,15 +1,34 @@
+from flask import Flask, render_template, request, current_app as app
 from flask_apscheduler import APScheduler
-from flask import Flask, render_template, request, current_app as current_app
 import sqlite3
+
 
 app = Flask(__name__)
 scheduler = APScheduler()
 scheduler.init_app(app)
 scheduler.start()
 
-@app.route('/', methods=['GET', 'POST'])
-def done():
-    task = request.form['task']
+
+@app.route('/')
+def reminder():
+    return render_template('tasks.html')
 
 
-    return render_template('tasks.html', task = task )
+@app.route('/task', methods=['GET', 'POST'])
+def form():
+    form = request.form['task']
+    date = request.form['calendar']
+    return render_template('tasks.html', task = form)
+
+
+@app.route('/all')
+def all():
+    conn = sqlite3.connect('./static/data/store.db')
+    curs = conn.cursor()
+    store = []
+    rows = curs.execute("SELECT * from store")
+    return render_template('tasks.html', store = store)
+
+
+if __name__ == '__main__':
+    app.run(debug = True, host='0.0.0.0')
